d815e2efb07b4c2e453d12f10dfb27ef0f718f00
camilohv <72526400+camilohv@users.noreply.github.com>
2022-03-08 00:21:06 -0500

    added styling for Home page

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -1,22 +1,47 @@
-import React from "react";
+import React, { useRef } from 'react';
+import './Home.css';
 import Button from 'react-bootstrap/Button';
 import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 
-function Home() {
+
+
+const  Home = props => {
+
+    // Reference to original input element
+    const hiddenFileInput = React.useRef(null);
+    
+    // Adds input functionality to desired element
+    const handleClick = event => {
+        hiddenFileInput.current.click();
+    };
+    // Function to handle the user selected file
+    const handleChange = event => {
+        const fileUploaded = event.target.files[0];
+        props.handleFile(fileUploaded);
+    };
+
     return (
-        <div>
-            <a>THIS IS SOME TEST TEXT LOL THIS IS THE HOME PAGE</a>
-            <div className="d-grid gap-2">
-                <Button variant="primary" size="lg">
+        <div className="homeContent">
+            <div id="ImageUploadButton">
+                <input
+                    type="file"
+                    ref={hiddenFileInput}
+                    onChange={handleChange}
+                    style={{display: 'none'}}
+                />
+                <Button variant="primary" size="lg" onClick={handleClick}>
                     Upload an Image
                 </Button>
             </div>
+            
             <InputGroup className="mb-3">
+                
                 <FormControl
                 aria-label="Example text with button addon"
                 aria-describedby="basic-addon1"
-                placeholder = "Enter Image URL..."
+                placeholder= "Enter Image URL..."
                 />
                 <Button variant="outline-secondary" id="button-addon1">
                 Search
@@ -24,8 +49,10 @@ function Home() {
             </InputGroup>
             
             
+                
+
+                
         </div>
-        
         
         
     );
