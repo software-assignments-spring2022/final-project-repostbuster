3aac7823e992ffc496fad022bc121606ff035406
camilohv <72526400+camilohv@users.noreply.github.com>
2022-03-07 21:26:27 -0500

    added home and FAQ route paths

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Header from './components/Header.js'
 import Results from './components/Results.js';
+import Home from './components/Home.js';
+import FAQ from './components/FAQ.js';
 
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
       <BrowserRouter>
         <Header/>
         <Routes>
+          <Route path="/home" element={<Home />}/>
+          <Route path="/faq" element={<FAQ />}/>
           <Route path="/results" element={<Results />}/>
         </Routes>
       </BrowserRouter>
