7e501e1d9c2efb26380da5c373c9b0693c2fb97c
camilohv <72526400+camilohv@users.noreply.github.com>
2022-03-07 22:00:30 -0500

    Added basic Image Upload Button and Search Field elements

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -1,8 +1,32 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
 
 function Home() {
     return (
-        <a>THIS IS SOME TEST TEXT LOL THIS IS THE HOME PAGE</a>
+        <div>
+            <a>THIS IS SOME TEST TEXT LOL THIS IS THE HOME PAGE</a>
+            <div className="d-grid gap-2">
+                <Button variant="primary" size="lg">
+                    Upload an Image
+                </Button>
+            </div>
+            <InputGroup className="mb-3">
+                <FormControl
+                aria-label="Example text with button addon"
+                aria-describedby="basic-addon1"
+                placeholder = "Enter Image URL..."
+                />
+                <Button variant="outline-secondary" id="button-addon1">
+                Search
+                </Button>
+            </InputGroup>
+            
+            
+        </div>
+        
+        
         
     );
 }
