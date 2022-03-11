e14a0a54b64cff80836e5b691bb486cada9bc9a4
Hyunjun Choi <guswns3723@gmail.com>
2022-03-10 19:36:59 -0500

    modified uploadImages.js App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,20 +1,25 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Header from './components/Header.js'
-import Results from './components/Results.js';
-
+import Header from "./components/Header.js";
+import Results from "./components/Results.js";
+import UploadImage from "./uploadImage.js";
 
 function App() {
-  return (
-    <div className="wrapper">
-      <BrowserRouter>
-        <Header/>
-        <Routes>
-          <Route path="/results" element={<Results />}/>
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className="wrapper">
+            <BrowserRouter>
+                <Header />
+                <Routes>
+                    <Route path="/results" element={<Results />} />
+                    <Route
+                        path="/uploadimage"
+                        element={<UploadImage />}
+                    ></Route>
+                    <Route name="searchSetting" path="/searchSetting" />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
---
+++ front-end/src/searchSetting.js
@@ -0,0 +1,9 @@
+// import "./searchSetting.css";
+
+// const searchSetting = (props) => {
+//     return (
+//         <div className="">
+//             <h1>{props.query.the}</h1>
+//         </div>
+//     );
+// };
