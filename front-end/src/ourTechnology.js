72c63e2857d63f02e2d8deb87f31e55930164909
Shrey Mittal <shreymittal@gmail.com>
2022-03-11 19:40:06 -0500

    Finished Implementing the our technology page

--- front-end/src/ourTechnology.js
+++ front-end/src/ourTechnology.js
@@ -18,12 +18,24 @@ const ourTechnology = () => {
                     The Google Custom Search JSON API allows applications to tap into the powerful Google image search
                     engine to garner either web search or image search results in a JSON file format, utilizing the
                     RESTful request API.
+                    https://developers.google.com/custom-search/v1/overview
                 </p>
             </section>
             <section>
-                <h1>Privacy Policy</h1>
+                <h2>Setting up the API</h2>
                 <p>
-
+                    For starters, in order to use the Programmable Search Engine, you must make a API key, to interace
+                    with Google's cloud computing service. Users are able to create a free level of the API key, which
+                    permits up to 100 searches a day, and it can then be scaled up from there to permit more searches
+                    on paid tiers.
+                </p>
+            </section>
+            <section>
+                <h2>API Operations</h2>
+                <p>
+                    The Programmable Search operations features relatively simple operations, with only the "list" call,
+                    which returns the search results. The returned data is in the form of JSON, which includes search
+                    metadata, PSE metadata, and the actual search results.
                 </p>
             </section>
         </div>
