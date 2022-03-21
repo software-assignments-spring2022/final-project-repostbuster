aa1194611793e0b9532aec145fead222a8fffb19
rileyv327 <riley.valls327@gmail.com>
2022-03-21 19:40:33 -0400

    added routes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,14 +2,19 @@ import './App.css';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Header from './components/Header.js'
 import Results from './components/Results.js';
-
+import About from './About.js'
+import UploadImage from './uploadImage';
+import Technology from './Technology';
 
 function App() {
   return (
-    <div className="wrapper">
+    <div>
       <BrowserRouter>
         <Header/>
         <Routes>
+          <Route path="/about" element={<About />}/>
+          <Route path="/tech" element={<Technology />}/>
+          <Route path="/upload" element={<UploadImage />}/>
           <Route path="/results" element={<Results />}/>
         </Routes>
       </BrowserRouter>
