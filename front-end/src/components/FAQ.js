563abb6da6f660f0a34b76ad390c29c7607a850b
camilohv <72526400+camilohv@users.noreply.github.com>
2022-03-24 15:48:57 -0400

    FAQ styling

--- front-end/src/components/FAQ.js
+++ front-end/src/components/FAQ.js
@@ -4,17 +4,29 @@ import './FAQ.css';
 function FAQ() {
     return (
         <div id = "content">
-            <h1>HEADER 1</h1>
+            <h1>Frequently Asked Questions</h1>
 
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus gravida iaculis posuere. Morbi at odio nisl. Praesent at hendrerit nisi. Curabitur placerat, massa et pellentesque elementum, quam leo sagittis ipsum, eu dignissim mauris lectus eu erat. Morbi aliquam sit amet felis id convallis. Proin elementum, libero sit amet luctus accumsan, ipsum mauris consectetur augue, vel ullamcorper nibh ex a eros. Etiam cursus, sapien eu bibendum consequat, ipsum metus lacinia dolor, at interdum risus diam a neque. In hac habitasse platea dictumst. Nunc imperdiet mauris eget semper venenatis. Aliquam ac laoreet diam.</p>
+            <h3>I found one of my images being used without my permission by someone else. What should I do?</h3>
+            <p>What you decide to do at this point is up to you.</p>
+            <p>You can can try to directly contact the person who reposted your image. If the image was reposted on a social media platform like Twitter, Instagram, or Facebook, you will most likely be able to submit a copyright complaint directly on these sites to have the reposted image be taken down. </p>
+            <p>Bellow are links to guides on how to file a copyright complaint on some popular social media platforms:</p>
 
-            <h1>HEADER 1</h1>
+            <li>
+                <a href="https://help.twitter.com/en/rules-and-policies/copyright-policy">Twitter's Copyright Policy</a> 
+            </li>
+            <li>
+                <a href="https://help.instagram.com/126382350847838">Instagram's Copyright Policy</a>
+            </li>
 
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus gravida iaculis posuere. Morbi at odio nisl. Praesent at hendrerit nisi. Curabitur placerat, massa et pellentesque elementum, quam leo sagittis ipsum, eu dignissim mauris lectus eu erat. Morbi aliquam sit amet felis id convallis. Proin elementum, libero sit amet luctus accumsan, ipsum mauris consectetur augue, vel ullamcorper nibh ex a eros. Etiam cursus, sapien eu bibendum consequat, ipsum metus lacinia dolor, at interdum risus diam a neque. In hac habitasse platea dictumst. Nunc imperdiet mauris eget semper venenatis. Aliquam ac laoreet diam.</p>
+            <h3>Why is RepostBuster unable to find any reposted instances of the image I uploaded?</h3>
+            <p>Your image has mostlikely not been reuploaded elsewhere on the internet. Which is a good thing!</p>
 
-            <h1>HEADER 1</h1>
+            <h3>RepostBuster is only returning images similar to the one I uploaded and isn't finding exact matches. Why?</h3>
+            <p></p>
 
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus gravida iaculis posuere. Morbi at odio nisl. Praesent at hendrerit nisi. Curabitur placerat, massa et pellentesque elementum, quam leo sagittis ipsum, eu dignissim mauris lectus eu erat. Morbi aliquam sit amet felis id convallis. Proin elementum, libero sit amet luctus accumsan, ipsum mauris consectetur augue, vel ullamcorper nibh ex a eros. Etiam cursus, sapien eu bibendum consequat, ipsum metus lacinia dolor, at interdum risus diam a neque. In hac habitasse platea dictumst. Nunc imperdiet mauris eget semper venenatis. Aliquam ac laoreet diam.</p>
+            <h2>Troubleshooting</h2>
+            <h3>X functionbality on RepostBuster isn't working properly. What Should I do?</h3>
+            <p>If you're having a problem with RepostBuster that is not addressed here, please contact us at [EMAIL].</p>
         </div>
         
         
