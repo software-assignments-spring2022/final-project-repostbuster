5a85b93c0a1d9370b666906bbc7fc11f2f6dd5cf
Hyunjun Choi <guswns3723@gmail.com>
2022-04-06 11:32:35 -0400

    include file upload functionality in Home.js

--- back-end/app.js
+++ back-end/app.js
@@ -8,25 +8,25 @@ const multer = require("multer"); // middleware to handle HTTP POST requests wit
 const axios = require("axios"); // middleware for making requests to APIs
 require("dotenv").config({ silent: true }); // load environmental variables from a hidden file named .env
 const morgan = require("morgan"); // middleware for nice logging of incoming HTTP requests
+const cors = require("cors");
 
 // use the morgan middleware to log all incoming http requests
-app.use(morgan("dev")); // morgan has a few logging default styles - dev is a nice concise color-coded style
+app.use(morgan("dev", { skip: (req, res) => process.env.NODE_ENV === "test" })); // log all incoming requests, except when in unit test mode.
+// morgan has a few logging default styles - dev is a nice concise color-coded style
 
 // use express's builtin body-parser middleware to parse any data included in a request
 app.use(express.json()); // decode JSON-formatted incoming POST data
-app.use(express.urlencoded({ extended: true })); // decode url-encoded incoming POST data
+app.use(express.urlencoded({ extended: false })); // decode url-encoded incoming POST data... should we leave it true????????????????
 
 // make 'public' directory publicly readable with static content
-app.use("/static", express.static("public"));
+const publicPath = path.join(__dirname, "public"); // instead of app.use("/static", express.static("public"));
+app.use(express.static(publicPath));
 
-const cors = require('cors')
-app.use(cors())
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Welcome to RepostBuster!");
 });
-
-
 
 // Dont forget to create unit tests for your respective functions!
 
@@ -41,32 +41,33 @@ app.get("/", (req, res) => {
 app.get("/results", (req, res) => {
     const body = [
         {
-        "source": "Holdlamis",
-        "date": "05/15/2021",
-        "link": "ucoz.com"
-      }, {
-        "source": "Namfix",
-        "date": "03/22/2021",
-        "link": "patch.com"
-      }, {
-        "source": "Alphazap",
-        "date": "01/21/2022",
-        "link": "soup.io"
-      }, {
-        "source": "Namfix",
-        "date": "02/16/2022",
-        "link": "feedburner.com"
-      }
+            source: "Holdlamis",
+            date: "05/15/2021",
+            link: "ucoz.com",
+        },
+        {
+            source: "Namfix",
+            date: "03/22/2021",
+            link: "patch.com",
+        },
+        {
+            source: "Alphazap",
+            date: "01/21/2022",
+            link: "soup.io",
+        },
+        {
+            source: "Namfix",
+            date: "02/16/2022",
+            link: "feedburner.com",
+        },
     ];
     // send the response as JSON text to the client
-    
-    res.json(body)
-});
 
+    res.json(body);
+});
 
 // Use Express to store user login credentials and preffered search settings for their profile
 // Duardo Akerele
-
 
 // export the express app we created to make it available to other modules
 module.exports = app; // CommonJS export style!
--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -1,70 +1,103 @@
-import React, {useRef} from 'react';
-import './Home.css';
-import Button from 'react-bootstrap/Button';
-import InputGroup from 'react-bootstrap/InputGroup';
-import FormControl from 'react-bootstrap/FormControl';
+import React, { useRef, useState, useEffect } from "react";
+import "./Home.css";
+import Button from "react-bootstrap/Button";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import axios from "axios";
 //import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-const  Home = props => {
-
-    // Reference to original input element
+const Home = (props) => {
+    /* // Reference to original input element
     const hiddenFileInput = React.useRef(null);
-    
+
     // Adds input functionality to desired element
-    const handleClick = event => {
+    const handleClick = (event) => {
         hiddenFileInput.current.click();
     };
     // Function to handle the user selected file
-    const handleChange = event => {
+    const handleChange = (event) => {
         const fileUploaded = event.target.files[0];
         //props.handleFile(fileUploaded);
+    }; */
 
-        
+    const [image, setImage] = useState([]);
+    const [error, setError] = userState("");
+
+    // function to handle the selected file
+    const uploadHandler = (event) => {
+        // create an instance of FormData
+        const data = new FormData();
+
+        // appending the file
+        data.append("file", event.target.files[0]);
+        // see the file details
+        console.log(event.target.files[0]);
+
+        axios
+            .post("/uploadImage", data)
+            .then((res) => {
+                setImage(res.data);
+            })
+            .catch((err) => {
+                console.log("ERROR!!!!!!!");
+                setError(err);
+            });
     };
 
     return (
         <div className="homeContent">
             <div>
                 <h1>Reverse Image Search</h1>
-                <p> A tool that uses pictures to find pictures on the internet</p>
+                <p>
+                    {" "}
+                    A tool that uses pictures to find pictures on the internet
+                </p>
                 <p></p>
-                <Button variant="secondary" size="lg" href= "HowItWorks"> 
+                <Button variant="secondary" size="lg" href="HowItWorks">
                     Learn how it works!
                 </Button>
             </div>
             <div></div>
 
             <div id="ImageUploadButton">
-                <input
+                {/* <input
                     type="file"
                     ref={hiddenFileInput}
-                    onChange={handleChange}
-                    style={{display: 'none'}}
+                    onChange={handleChange} // File Explorer pops up
+                    style={{ display: "none" }}
+                /> */}
+
+                {/* button  */}
+                <input
+                    type="file"
+                    name="imageFile"
+                    onChange={this.uploadHandler}
                 />
-                <Button class="uploadImageBtn" href= "uploadImage" variant="primary" size="lg" onClick={handleClick}>
+
+                <Button
+                    class="uploadImageBtn"
+                    href="uploadImage"
+                    variant="primary"
+                    size="lg"
+                    onClick={handleClick} // to /uploadImage
+                >
                     Upload an Image
                 </Button>
             </div>
-            
+
             <InputGroup className="mb-3">
-                
                 <FormControl
-                aria-label="Example text with button addon"
-                aria-describedby="basic-addon1"
-                placeholder= "Enter Image URL..."
+                    aria-label="Example text with button addon"
+                    aria-describedby="basic-addon1"
+                    placeholder="Enter Image URL..."
                 />
-                
+
                 <Button variant="outline-secondary" id="button-addon1">
-                Search
+                    Search
                 </Button>
             </InputGroup>
-
-                
         </div>
-        
-        
     );
-}
+};
 
 export default Home;
