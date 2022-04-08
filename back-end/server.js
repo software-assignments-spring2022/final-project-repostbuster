8b1faa530f965f2b2dfa2b3f11340b6d62501d6e
Hyunjun Choi <guswns3723@gmail.com>
2022-04-08 17:30:56 -0400

    reverted server.js back

--- back-end/server.js
+++ back-end/server.js
@@ -1,21 +1,25 @@
 #!/usr/bin/env node
 
 // import the express app
-const express = require("express");
-const app = express();
-const bodyParser = require('body-parser');
+const server = require("./app");
+//const express = require("express");
+//const app = express();
 
 // which port to listen for HTTP(S) requests
-const port = 3001;
+const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
- 
+const listener = server.listen(port, function () {
+    console.log(`Server running on port ${port}`);
+});
 
+const close = () => {
+    listener.close();
+};
+/*
+app.use(express.static("front-end\\public"));
 app.listen(port, () => {
     console.log("listening on "+port);
 });
-
 //create an account
 app.post('/register', (req, res) => {
     const username = req.body.email;
@@ -23,20 +27,15 @@ app.post('/register', (req, res) => {
     const password = req.body.password;
     //pass fields into database
 });
-
 app.post('/login', (req, res) => {
-
     const username = req.body.email;
     const password = req.body.password;
     //check username & pass agaisnt database entry
-    console.log(req.body);
-    res.send("SUCCESS");
     //if match return success page
 });
-
-
+*/
 module.exports = {
     close: close,
-}
+};
 // export the close function
 //module.exports = app;
