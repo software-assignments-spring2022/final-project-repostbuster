b1d5a6e27767b62385e85dc46cfffd4fedaedfcb
Hyunjun Choi <guswns3723@gmail.com>
2022-05-02 11:11:20 -0400

    button style

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -4,10 +4,22 @@ import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
 import axios from "axios";
 import "../styles.css";
+import styled from "styled-components";
 //import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Home = (props) => {
     const [image, setImage] = useState(null);
+
+    const Button = styled.button`
+        background: ${(props) => (props.primary ? "palevioletred" : "white")};
+        color: ${(props) => (props.primary ? "white" : "palevioletred")};
+
+        font-size: 1em;
+        margin: 1em;
+        padding: 0.25em 1em;
+        border: 2px solid palevioletred;
+        border-radius: 3px;
+    `;
 
     const handleClick = () => {
         axios.post("http://localhost:3000/home", image).then((res) => {
@@ -54,7 +66,9 @@ const Home = (props) => {
                     ></input>
                 </div>
                 <div>
-                    <button type="submit">Submit</button>
+                    {/* <button type="submit">Submit</button> */}
+                    <Button>Upload a file</Button>
+                    <input type="file" style={{ display: "none" }} />
                 </div>
             </form>
         </div>
